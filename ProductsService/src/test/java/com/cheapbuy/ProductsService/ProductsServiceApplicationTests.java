52eae6b0abb6d808f71c63ee1176feb35696ff0e
Adil Mansoor <adilmnr27@gmail.com>
2022-04-26 14:22:50 +0530

    init: First Commit For Products Service

---
+++ ProductsService/src/test/java/com/cheapbuy/ProductsService/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheapbuy.ProductsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
