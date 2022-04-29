5837f6f77099e6844c93f58c65ccfd890354bd47
Adil Mansoor <adilmnr27@gmail.com>
2022-04-29 09:32:14 +0530

    init: Orders Service component initialized

---
+++ OrdersService/src/test/java/com/cheapbuy/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheapbuy.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
