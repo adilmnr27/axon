77204f52629fab365ef4b178e6280dbebd75d3d7
Adil Mansoor <adilmnr27@gmail.com>
2022-05-01 10:08:50 +0530

    feat: Hack to solve the ForbiddenClassCastException occuring because of serialization

--- OrdersService/src/main/java/com/cheapbuy/ordersservice/OrdersServiceApplication.java
+++ OrdersService/src/main/java/com/cheapbuy/ordersservice/OrdersServiceApplication.java
@@ -3,6 +3,8 @@ package com.cheapbuy.ordersservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import com.thoughtworks.xstream.XStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,20 @@ public class OrdersServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
 	}
+	
+    /**
+     Hack to solve the ForbiddentClassCastException occuring becuase of serilaizaion problem.
+     Obviously not to be used in production
+     */
+    @Bean
+    public XStream xStream() {
+        XStream xStream = new XStream();
+      
+        xStream.allowTypesByWildcard(new String[] {
+                "com.cheapbuy.**"
+        });
+        return xStream;
+    }
+	
 
 }
