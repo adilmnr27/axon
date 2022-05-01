9f37768821a4339b865f49bb5c23bc6c5428a649
Adil Mansoor <adilmnr27@gmail.com>
2022-05-01 10:40:14 +0530

    feat: Hack to bypass Forbidden CLass Exception. Moved to Separate Config file.

--- OrdersService/src/main/java/com/cheapbuy/ordersservice/OrdersServiceApplication.java
+++ OrdersService/src/main/java/com/cheapbuy/ordersservice/OrdersServiceApplication.java
@@ -3,8 +3,6 @@ package com.cheapbuy.ordersservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import com.thoughtworks.xstream.XStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,20 +11,6 @@ public class OrdersServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
 	}
-	
-    /**
-     Hack to solve the ForbiddentClassCastException occuring becuase of serilaizaion problem.
-     Obviously not to be used in production
-     */
-    @Bean
-    public XStream xStream() {
-        XStream xStream = new XStream();
-      
-        xStream.allowTypesByWildcard(new String[] {
-                "com.cheapbuy.**"
-        });
-        return xStream;
-    }
 	
 
 }
