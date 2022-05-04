499bc5c5456cc73913c5c75c62b7a525ac450821
Adil Mansoor <adilmnr27@gmail.com>
2022-05-04 13:29:25 +0530

    init: init commit for payments service component

---
+++ PaymentsService/src/main/java/com/cheapbuy/paymentsservice/core/configuration/AxonConfig.java
@@ -0,0 +1,26 @@
+package com.cheapbuy.paymentsservice.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.thoughtworks.xstream.XStream;
+
+@Configuration
+public class AxonConfig {
+
+	
+	 /**
+    Hack to solve the com.thoughtworks.xstream.security.ForbiddenClassException occuring becuase of serilaizaion problem.
+    Obviously not to be used in production
+    */
+   @Bean
+   public XStream xStream() {
+       XStream xStream = new XStream();
+     
+       xStream.allowTypesByWildcard(new String[] {
+               "com.cheapbuy.**"
+       });
+       return xStream;
+   }
+   
+}
